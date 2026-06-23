d7bdf67516c8868e34b5362ae0419586ea100b49
Conroks <Connor_r@yahoo.com>
2026-06-22 22:27:19 -0400

    Prayer/attack checker - Ranged/Melee
    Implemented a check for Hunllef's attack types and the current prayer and records total damage received while praying correctly and when not praying/praying incorrectly in variables. Tracks attack types and Hullef's state based on animation changes. Melee is still WIP

--- gauntletcoach/GauntletCoachPlugin.java
+++ gauntletcoach/GauntletCoachPlugin.java
@@ -14,26 +14,39 @@ import net.runelite.api.Hitsplat;
 import java.util.Set;
 import net.runelite.client.config.ConfigManager;
 import com.google.inject.Provides;
+import net.runelite.api.Prayer;
 
 @PluginDescriptor(
         name = "Gauntlet Coach")
 
-public class GauntletCoachPlugin extends Plugin
-{
+public class GauntletCoachPlugin extends Plugin {
     @Inject
     private Client client;
-
-    private static final Set<Integer> GAUNTLET_NPC_IDS = Set.of(
-            9021, 9026, 9027, 9028, 9029, 9030, 9031, 9032, 9033, 9034, // Gauntlet
-            9035, 9040, 9041, 9042, 9043, 9044, 9045, 9046, 9047, 9048  // Corrupted
+    //initializes the set of relevant npc id's
+    private static final Set<Integer> HUNLLEF_IDS = Set.of(
+            9021, // Crystal Hunllef ID
+            9035, 9036, 9037  // Corrupted Hunllef ID
     );
+
+    private static final Set<Integer> TORNADO_IDS = Set.of(
+            9025, //Regular Gauntlet
+            9039  //Corrupted Gauntlet
+    );
+
+    private static final Set<Integer> MINION_IDS = Set.of(
+            9026, 9027, 9028, 9029, 9030, 9031, 9032, 9033, 9034, //Regular Gauntlet
+            9040, 9041, 9042, 9043, 9044, 9045, 9046, 9047, 9048  //Corrupted Gauntlet
+    );
+
 
     @Inject
     private GauntletCoachConfig config;
-    /*
+
+    /* Relevant asset ID tracking
     ~~~~~~~~~~~~~~Gauntlet~~~~~~~~~~~~~~~
 
     Hunllef Id: 9021
+    Hunllef Tornados: 9025
     Crystal Rat: 9026
     Crystal Bat: 9028
     Crystal Spider: 9027
@@ -46,7 +59,8 @@ public class GauntletCoachPlugin extends Plugin
 
     ~~~~~~~~~~~Corrupted Gauntlet~~~~~~~~~~~~~
 
-    Corrupted Hunllef Id: 9035
+    Corrupted Hunllef Id: 9035/9036/9037
+    Hunllef Tornados : 9039
     Corrupted Rat: 9040
     Corrupted Bat: 9042
     Corrupted Spider: 9041
@@ -57,28 +71,54 @@ public class GauntletCoachPlugin extends Plugin
     Corrupted Dragon: 9047
     Corrupted Bear: 9046
 
+    Animation ID tracking
+        Hunllef
+             Hunllef attack:8419
+             Hunllef Melee attack:8420
+             Hunllef Ranged to Mage swap animation: 8754
+             Hunllef Mage to Ranged swap animation: 8755
+        */
 
-     */
-    private int totalDamage = 0;
-    private int attackCount = 0;
+    private int hunllefDamageDealt = 0;
+    private int hunllefDamageReceived = 0;
+    private int hunllefAttackCount = 0;
 
+    private int minionDamageDealt = 0;
+    private int minionDamageReceived = 0;
+    private int minionAttackCount = 0;
+
+    private enum HunllefStyle {MAGE,RANGED,MELEE}
+    private HunllefStyle currentHunllefStyle = HunllefStyle.RANGED;
+    private boolean lastAttackOnPrayer = false;
+
+    private static final int hunllefAttackAnimation = 8419;
+    private static final int hunllefMageSwitchAnimation = 8754;
+    private static final int hunllefRangedSwitchAnimation = 8755;
+    private static final int hunllefMeleeAttackAnimation = 8420;
+
+    private int hunllefDamageReceivedOnPrayer = 0;
+    private int hunllefDamageReceivedOffPrayer = 0;
+
+    //Initialize the plugin
     @Override
-    protected void startUp()
-    {
-        totalDamage = 0;
-        attackCount = 0;
+    protected void startUp() {
+        hunllefDamageDealt = 0;
+        hunllefDamageReceived = 0;
+        hunllefAttackCount = 0;
+        minionDamageDealt = 0;
+        minionDamageReceived = 0;
+        minionAttackCount = 0;
         System.out.println("Gauntlet Coach started");
     }
 
     @Override
-    protected void shutDown()
-    {
+    protected void shutDown() {
         System.out.println("Gauntlet Coach stopped");
     }
 
+    //When NPCs are rendered in, reports the NPC Id number to the terminal
     @Subscribe
-    public void onNpcSpawned(NpcSpawned event)
-    {
+    public void onNpcSpawned(NpcSpawned event) {
         NPC npc = event.getNpc();
 
         System.out.println(
@@ -89,42 +129,92 @@ public class GauntletCoachPlugin extends Plugin
         );
     }
 
+    //Connects config file
     @Provides
-    GauntletCoachConfig provideConfig(ConfigManager configManager)
-    {
+    GauntletCoachConfig provideConfig(ConfigManager configManager) {
         return configManager.getConfig(GauntletCoachConfig.class);
     }
 
+    //Tracks Hitsplats and sums damage, outputting to terminal
     @Subscribe
-    public void onHitsplatApplied(HitsplatApplied event)
-    {
+    public void onHitsplatApplied(HitsplatApplied event) {
         Actor actor = event.getActor();
-        if (!(actor instanceof NPC)) return;
-
-        NPC npc = (NPC) actor;
-        if (!GAUNTLET_NPC_IDS.contains(npc.getId())) return;
-
         Hitsplat hitsplat = event.getHitsplat();
-        if (hitsplat.isMine())
-        {
-            totalDamage += hitsplat.getAmount();
-            System.out.println("Damage dealt: " + hitsplat.getAmount() + " | Total: " + totalDamage);
+
+        if(actor == client.getLocalPlayer()) {
+            if(lastAttackOnPrayer) {
+                hunllefDamageReceivedOnPrayer += hitsplat.getAmount();
+                System.out.println("Damage received ON correct prayer: " + hitsplat.getAmount());
+            }else {
+                hunllefDamageReceivedOffPrayer += hitsplat.getAmount();
+                System.out.println("Change Prayer, Damage received during incorrect Prayer: " + hitsplat.getAmount());
+            }
+        }
+
+        if ((actor instanceof NPC)) {
+            NPC npc = (NPC) actor;
+
+            //Test to see NPC ID when hitting creature ~~~~DEBUGGING + INFO GATHERING
+            //System.out.println("Hit NPC ID: " + npc.getId() + " Name: " + npc.getName() + " Amount: " + hitsplat.getAmount() + " isMine: " + hitsplat.isMine());
+
+            if (hitsplat.isMine()) {
+                if (HUNLLEF_IDS.contains(npc.getId())) {
+                    hunllefDamageDealt += hitsplat.getAmount();
+                    System.out.println("Damage to Hunllef: " + hitsplat.getAmount() + " ~ Total: " + hunllefDamageDealt);
+                }
+            }
         }
     }
 
+    //Checks players Prayer against hunllef's current attack and outputs
+    private void checkHunllefAttack(HunllefStyle style) {
+        boolean onPrayer = false;
+
+        if(style == HunllefStyle.RANGED) {
+            onPrayer = client.isPrayerActive(Prayer.PROTECT_FROM_MISSILES);
+        } else if (style == HunllefStyle.MAGE) {
+            onPrayer = client.isPrayerActive(Prayer.PROTECT_FROM_MAGIC);
+        } else if (style == HunllefStyle.MELEE){
+            onPrayer = client.isPrayerActive(Prayer.PROTECT_FROM_MELEE);
+        }
+
+        lastAttackOnPrayer = onPrayer;
+        hunllefAttackCount++;
+
+        System.out.println("Hunllef attacked with " + style + " ~ Player on prayer: " + onPrayer);
+
+    }
+
+    //Tracks NPC animation changes
     @Subscribe
-    public void onAnimationChanged(AnimationChanged event)
-    {
+    public void onAnimationChanged(AnimationChanged event) {
         Actor actor = event.getActor();
         if (!(actor instanceof NPC)) return;
 
         NPC npc = (NPC) actor;
-        if (!GAUNTLET_NPC_IDS.contains(npc.getId())) return;
+        if (!HUNLLEF_IDS.contains(npc.getId())) return;
 
+        int animID = npc.getAnimation();
+
+        if (animID == hunllefRangedSwitchAnimation) {
+            currentHunllefStyle = HunllefStyle.RANGED;
+            System.out.println("Hunllef swapped to RANGED");
+        } else if (animID == hunllefMageSwitchAnimation) {
+            currentHunllefStyle = HunllefStyle.MAGE;
+            System.out.println("Hunllef swapped to MAGE");
+        } else if (animID == hunllefMeleeAttackAnimation) {
+            //checkHunllefAttack(HunllefStyle.MELEE);
+            System.out.println("Hunllef hit with MELEE");
+        } else if (animID == hunllefAttackAnimation) {
+            checkHunllefAttack(currentHunllefStyle);
+        }
+/*
+        //Outputs animation change details to terminal ~~~~DEBUGGING + INFO GATHERING
         System.out.println(
                 "NPC Animation: " + npc.getName() +
-                        " AnimID: " + npc.getAnimation()
+                " AnimID: " + npc.getAnimation()
         );
-    }
+*/
 
+    }
 }
